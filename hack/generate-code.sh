4fe33ae9aca76408e874b14143c07d9d49ece510
Maximilian Rink <maximilian.rink@telekom.de>
2026-03-02 15:50:15 +0100

    fix: remove non-CRD manifests from Helm chart crds/ directory
    generate-code.sh was copying all YAML files from doc/crds/ into the
    Helm chart crds/ directory, including deployment manifests (daemonset,
    operator, webhook). Helm crds/ only accepts CustomResourceDefinition
    objects, causing helm lint to fail.
    
    Narrow the glob to whereabouts.cni.cncf.io_*.yaml and remove the
    spurious files.

--- hack/generate-code.sh
+++ hack/generate-code.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(pwd)
 ${BASEDIR}/bin/controller-gen object crd:crdVersions=v1 paths="./pkg/api/..." output:crd:artifacts:config=doc/crds
-cp doc/crds/*.yaml deployment/whereabouts-chart/crds/
+cp doc/crds/whereabouts.cni.cncf.io_*.yaml deployment/whereabouts-chart/crds/
